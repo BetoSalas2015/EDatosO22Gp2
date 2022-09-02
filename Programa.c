f86493fc3ae61e897134eff36bd32e6b4f49158b
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2022-09-02 16:55:55 -0500

    Sesión 9 - 03/09/2022

--- Programa.c
+++ Programa.c
@@ -1,16 +1,23 @@
+//  Sesión 9 - 03/09/2022
 #include <stdio.h>		//  Para la i/o del programa
 #include <stdlib.h>		//  Para system()
 #include <string.h>		//  Para usar srtcpy()
 
 #define pausa system("pause")
 
+struct Persona
+{
+	char nombre[15];
+	char apellido[15];
+};
+
 struct datos				// Defino la estructura (struct) datos
-	{ 	
-		char nombre[30];	//  Defino los campos de la estructura persona (variables)
-		int edad;	
-		char sexo; 	
-		char direccion[30];
-	};
+{ 	
+	struct Persona NOMBRE;	//  Defino los campos de la estructura persona (variables)
+	int edad;	
+	char sexo; 	
+	char direccion[30];
+};
 
 int main()
 {
@@ -19,10 +26,11 @@ int main()
 
 	Agenda.edad = 25;
 	Agenda.sexo = 'H';
-	strcpy(Agenda.nombre, "Roberto Salazar");
+	strcpy(Agenda.NOMBRE.nombre, "Roberto");
+	strcpy(Agenda.NOMBRE.apellido, "Salazar");
 	strcpy(Agenda.direccion, "2 sur 2");
 
-	printf("Nombre: %s, \nEdad: %d, \nSexo: %c, \nDireccion: %s\n\n", Agenda.nombre, Agenda.edad, Agenda.sexo, Agenda.direccion);
+	printf("Nombre: %s %s, \nEdad: %d, \nSexo: %c, \nDireccion: %s\n\n", Agenda.NOMBRE.nombre, Agenda.NOMBRE.apellido, Agenda.edad, Agenda.sexo, Agenda.direccion);
 
 	printf("Dame la edad: ");
 	scanf("%d", &Agenda.edad);
@@ -31,11 +39,19 @@ int main()
 	scanf("%c", &Agenda.sexo);
 	fflush(stdin);
 	printf("Dame el nombre: ");
-	gets(Agenda.nombre);
+	gets(Agenda.NOMBRE.nombre);
+	printf("Dame el apellido: ");
+	gets(Agenda.NOMBRE.apellido);
 	printf("Dame la dirección: ");
 	gets(Agenda.direccion);
 
-	printf("Nombre: %s, \nEdad: %d, \nSexo: %c, \nDireccion: %s\n", Agenda.nombre, Agenda.edad, Agenda.sexo, Agenda.direccion);
+	printf("Nombre: %s %s, \nEdad: %d, \nSexo: %c, \nDireccion: %s\n\n", Agenda.NOMBRE.nombre, Agenda.NOMBRE.apellido, Agenda.edad, Agenda.sexo, Agenda.direccion);
+
+	printf("El char ocupa %d bytes en memoria. \n", sizeof( char ) );  
+	printf("El int ocupa %d bytes en memoria. \n", sizeof( int ) ); 
+	printf("El float ocupa %d bytes en memoria. \n", sizeof( float ) ); 
+	printf("El double ocupa %d bytes en memoria. \n", sizeof( double ) ); 
+	printf("El struct datos ocupa %d bytes en memoria. \n", sizeof( struct datos ) ); 
 
 	pausa;
 	return 0;
