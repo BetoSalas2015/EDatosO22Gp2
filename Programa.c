e78efb453fbf1ee45d2b5dcf29f363d49d1629f7
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2022-10-07 17:24:07 -0500

    Sesión 23 - 07/10/2022

--- Programa.c
+++ Programa.c
@@ -1,19 +1,31 @@
-//  Sesión 21 - 05/10/2022   -- Ordenamientos y búsquedas
+//  Sesión 23 - 07/10/2022   -- Ordenamientos y búsquedas
 #include <stdio.h>		//  Para la i/o del programa
 #include <stdlib.h>		//  Para system()
+#include <string.h>		//  Para el manejo de cadenas
 
 #define pausa system("pause")
 #define cls system("cls")
-#define SIZE 5
+#define SIZE 80
 
 int main()
 {
 	int costo = 60;			//  Declaro una variable entera llamada costo
-	//  Declaro un arreglo de 15 variables enteras-
-	int arreglo[SIZE] = { 0, 1, 2, 3, 4 };		
+	int i, cont;
+	char cadena[SIZE];		//  Declaro una cadena
+	printf("Dame una cadena: ");
+	gets(cadena);
 	
-	arreglo[] = 500;
-
+	i = 0; cont = 1;
+	while(cadena[i] != '\0')
+	{
+		printf("Cadena[%d] = '%c' \n", i, cadena[i]);
+		
+		if( cadena[i] >= 'a' && cadena[i] <= 122 )
+			cadena[i] = cadena[i] - 32;
+		i++; 
+	}
+	printf("La cadena tiene %d caracteres alfabeticos.\n", cont);
+	printf("%s\n", cadena);
 	pausa;
 	return 0;
 }
