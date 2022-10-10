502380814cdca872c0f3e222080aa5efc4886d56
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2022-10-10 17:23:59 -0500

    Sesión 24 - 10/10/2022

--- Programa.c
+++ Programa.c
@@ -1,31 +1,45 @@
-//  Sesión 23 - 07/10/2022   -- Ordenamientos y búsquedas
+//  Sesión 24 - 10/10/2022   -- Plantilla de Ordenamientos
 #include <stdio.h>		//  Para la i/o del programa
-#include <stdlib.h>		//  Para system()
-#include <string.h>		//  Para el manejo de cadenas
+#include <stdlib.h>		//  Para system() y para los numeros aleatorios
+#include <time.h>		//  PAra la función time()
 
 #define pausa system("pause")
 #define cls system("cls")
-#define SIZE 80
+
+#define SIZE 15			//  Tamaño de mi archivo de Datos
+
+void imprimeArreglo(int arr[])
+{
+	int i;
+	for(i = 0; i < SIZE; i++)
+		printf("%d, ", arr[i]);
+	putchar('\n');
+}
+
+// Programe su algoritmo de Ordenamiento aquí
+void nombreAlgoritmo(int arr[])				//  CAMBIE el nombre de la función
+{
+	//  Escriba su algoritmo de ordenamiento aquí
+}
 
 int main()
 {
-	int costo = 60;			//  Declaro una variable entera llamada costo
-	int i, cont;
-	char cadena[SIZE];		//  Declaro una cadena
-	printf("Dame una cadena: ");
-	gets(cadena);
+	int arreglo[SIZE], i;
+
+	srand( time(0) );				//  Seed RANDom - Semilla de numeros aleatorios
+
+	for(i = 0; i < SIZE; i++)
+		arreglo[i] = rand() % 1000;
+
+	printf("Arreglo desordenado: \n");
+	imprimeArreglo(arreglo);
+
+	//  Invoque su algoritmo de ordenamiento aquí
+	nombreAlgoritmo(arreglo);
+
+	printf("Arreglo ordenado: \n");
+	imprimeArreglo(arreglo);
 	
-	i = 0; cont = 1;
-	while(cadena[i] != '\0')
-	{
-		printf("Cadena[%d] = '%c' \n", i, cadena[i]);
-		
-		if( cadena[i] >= 'a' && cadena[i] <= 122 )
-			cadena[i] = cadena[i] - 32;
-		i++; 
-	}
-	printf("La cadena tiene %d caracteres alfabeticos.\n", cont);
-	printf("%s\n", cadena);
 	pausa;
 	return 0;
 }
