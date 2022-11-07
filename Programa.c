15d27fa6d78c54bb8e97fbb8fa574473dd5ae500
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2022-11-07 17:24:15 -0600

    Sesión 35 - 07/11/2022

--- Programa.c
+++ Programa.c
@@ -1,9 +1,12 @@
-//  Sesión 34 - 04/11/2022   -- Apuntadores
+//  Sesión 35 - 07/11/2022   -- Apuntadores
 #include <stdio.h>		//  Para la i/o del programa
 #include <stdlib.h>		//  Para system() y para los numeros aleatorios
 
 #define pausa system("pause")
 #define cls system("cls")
+
+int square(int *num);
+
 
 int main()
 {
@@ -19,20 +22,56 @@ int main()
 	//printf("El contendo de pointer es %d \n", *pointer);
 
 	//*pointer = 100;
-	int a, b, res;
-	int *pa, *pb, *pc;
+	//int a, b, res;
+	//int *pa, *pb, *pc;
 
-	pa = &a; pb = &b; pc = &res; 
+	//pa = &a; pb = &b; pc = &res; 
 
-	printf("Dame el primer numero: ");
-	scanf("%d", pa );
-	printf("Dame el segundo numero: ");
-	scanf("%d", pb );
+	//printf("Dame el primer numero: ");
+	//scanf("%d", pa );
+	//printf("Dame el segundo numero: ");
+	//scanf("%d", pb );
 
-	*pc = *pa + *pb;
+	//*pc = *pa + *pb;
 
-	printf("El resultado es: %d\n", *pc);
+	//printf("El resultado es: %d\n", *pc);
+
+	int *pointer,i;
+	int	arreglo[5]; 
+	int *ptrarr[5];			//  Declaro un arreglo de Apuntadores a entero
+
+	printf("El tamaño de el apuntador a enteros es: %d\n", sizeof(int *) );
+	printf("El tamaño de el apuntador a enteros es: %d\n", sizeof(double *) );
+	printf("El tamaño de el apuntador a enteros es: %d\n", sizeof(char *) );
+
+	printf("La dirección de Memeoria de arreglo[0] es %p\n", &arreglo[0]);
+	printf("La dirección de Memeoria de arreglo[1] es %p\n", &arreglo[1]);
+	printf("La dirección de Memeoria de arreglo[2] es %p\n", &arreglo[2]);
+	printf("La dirección de Memeoria de arreglo[3] es %p\n", &arreglo[3]);
+	printf("La dirección de Memeoria de arreglo[4] es %p\n", &arreglo[4]);
+
+	pointer = arreglo;		// Especifico el elemento del arreglo al cual voy a apuntar
+
+	printf("LA dirección guardada en pointer es %p\n", pointer);
+
+	//-------------------------------------------------------------
+	 
+	for(i = 0; i < 5; i++)
+	{
+		ptrarr[i] = &arreglo[i];
+		printf("LA dirección guardada en arrptr[%d] es %p\n", i, ptrarr[i]);
+	}
+	
+	i = 5;
+	square(&i);
+
+	printf("i = %d\n", i);
 
 	pausa;
 	return 0;
+}
+
+int square(int *num)
+{
+	return num + (*num++);
 }
