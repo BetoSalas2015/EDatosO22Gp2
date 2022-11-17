3d5ce076c8561f8576517740206802537dc8b268
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2022-11-16 18:01:50 -0600

    Sesión 39 - 16/11/2022

--- Programa.c
+++ Programa.c
@@ -1,6 +1,7 @@
-//  Sesión 38 - 14/11/2022   -- Apuntadores
+//  Sesión 39 - 16/11/2022   -- Cadenas
 #include <stdio.h>		//  Para la i/o del programa
 #include <stdlib.h>		//  Para system() y para los numeros aleatorios
+#include <string.h>		//  Para el manejo de cadenas
 
 #define pausa system("pause")
 #define cls system("cls")
@@ -82,27 +83,43 @@ int main()
 	//printf("La dirección guardada en pointer es %p\n", pointer);
 	//printf("La dirección guardada en pointer + 1 es %p\n", pointer + 1);
 	//printf("La dirección guardada en pointer + 2 es %p\n", pointer + 2);*/
-	char cadena[80];		// Estoy creando una cadena de 80 caracteres
-	char *ptr, *ptr2;
-	int cont = 0;
-	printf("Dame una cadena: ");
-	gets(cadena);
-	ptr = ptr2 = cadena;					//  Me posiciono en el primer caracter de la cadena
-	while( *ptr2 != '\0')
-	{	
-		ptr2++;
-	}
-	ptr2--;
-	while( ptr <= ptr2)
-	{
-		if(*ptr != *ptr2) {
-			printf("La cadena no es palindroma\n" );
-			pausa;
-			return 1;
-		}
-		ptr++; ptr2--;
-	}
-	printf("La cadena es palindroma\n");
+	//char cadena[80];		// Estoy creando una cadena de 80 caracteres
+	//char *ptr, *ptr2;
+	//int cont = 0;
+	//printf("Dame una cadena: ");
+	//gets(cadena);
+	//ptr = ptr2 = cadena;					//  Me posiciono en el primer caracter de la cadena
+	//while( *ptr2 != '\0')
+	//{	
+	//	ptr2++;
+	//}
+	//ptr2--;
+	//while( ptr <= ptr2)
+	//{
+	//	if(*ptr != *ptr2) {
+	//		printf("La cadena no es palindroma\n" );
+	//		pausa;
+	//		return 1;
+	//	}
+	//	ptr++; ptr2--;
+	//}
+	//printf("La cadena es palindroma\n");
+	char cadena1[80] = "Saludos programas!!!!";
+	char cadena2[80] = "Saludos mundo cruel!";
+	char *ptr;
+
+	//strcpy( cadena1, "Otra cadena");
+
+	puts(cadena1);
+
+	printf("El tamaño de la cadena 1 es %d\n", strlen(cadena1) );
+
+	ptr = cadena1 + strlen(cadena1) - 1;
+	printf("*ptr = %c \n", *ptr);
+
+	printf("strcmp(cadena1, cadena1) == %d\n", strcmp(cadena1, cadena1) );
+	printf("strcmp(cadena1, cadena2) == %d\n", strcmp(cadena1, cadena2) );
+ 
 	pausa;
 	return 0;
 }
